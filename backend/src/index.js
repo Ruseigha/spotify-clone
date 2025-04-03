a10cd757a85dd8daf9980c4b0cc235d8e423a6b5
Seigha Rufus <excellenceseigha@gmail.com>
2025-04-03 21:34:42 +0100

    add routes to the backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
+import adminRouter from "./routes/admin.route.js"
+import songsRouter from "./routes/songs.route.js"
+import albumsRouter from "./routes/albums.route.js"
+import statsRouter from "./routes/stats.route.js"
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT;
 
+app.use("/api/users", userRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/songs", songsRouter);
+app.use("/api/albums", albumsRouter);
+app.use("/api/stats", statsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running om port ${PORT}`);
