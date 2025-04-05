12de8f7743c3996d2ab47b91aad37dbe793a53e8
Seigha Rufus <excellenceseigha@gmail.com>
2025-04-05 21:33:03 +0100

    add getAllUsers controller to codebase

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { protectRoute } from "../middlewares/auth.middleware.js";
+import { getAllUsers } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("get user routes")
-});
+router.get("/", protectRoute, getAllUsers);
+// todo: add getMessage route
+
 
 export default router;
