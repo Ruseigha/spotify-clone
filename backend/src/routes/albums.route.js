75e85fb07a2c2dbcd613c1109fac74f07ea0111e
Seigha Rufus <excellenceseigha@gmail.com>
2025-04-05 21:09:07 +0100

    add all songs routes and controllers to codebase

--- backend/src/routes/albums.route.js
+++ backend/src/routes/albums.route.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import { getAllAlbums, getAlbumById } from "../controllers/album.controller.js";
 
 
 const router = Router();
+
+router.get("/", getAllAlbums);
+router.get("/:id", getAlbumById);
 
 export default router;
