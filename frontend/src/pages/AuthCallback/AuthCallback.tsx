b3a7cbf7fd6b4ab45f1103c732295578f8d8a952
Seigha Rufus <excellenceseigha@gmail.com>
2025-04-06 16:11:36 +0100

    complete authentication

--- frontend/src/pages/AuthCallback/AuthCallback.tsx
+++ frontend/src/pages/AuthCallback/AuthCallback.tsx
@@ -1,8 +1,34 @@
 import { Card, CardContent } from '@/components/ui/card';
+import { axiosInstance } from '@/lib/axios';
+import { useUser } from '@clerk/clerk-react';
 import { Loader } from 'lucide-react';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+
 
 const AuthCallback = () => {
+  const { isLoaded, user } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const syncUser = async () => {
+      try {
+        if(!isLoaded || !user) return;
+        await axiosInstance.post("/auth/callback", {
+          id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          imageUrl: user.imageUrl,
+        })
+      } catch (error) {
+        console.log("Error in auth callback", error);
+      } finally {
+        navigate("/");
+      }
+    };
+
+    syncUser();
+  },[isLoaded, user, navigate]);
   return (
     <div className='h-screen flex items-center justify-center bg-black h-full'>
       <Card className='w-[90%] max-w-md bg-zinc-900 border-zinc-800'>
