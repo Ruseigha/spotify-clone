a091fb9b08eda42f40bf05646b8a84c9d3aa1758
Seigha Rufus <excellenceseigha@gmail.com>
2025-04-06 17:00:28 +0100

    add Mainlayout to codebase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,20 @@ import HomePage from "./pages/HomePage/HomePage.tsx";
 import AuthCallback from "./pages/AuthCallback/AuthCallback.tsx";
 import "./App.css";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./Layout/MainLayout.tsx";
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />}/>
         <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpFallbackRedirectUrl={`/auth-callback`} />}/>
         <Route path="/auth-callback" element={<AuthCallback />}/>
+
+        <Route element={<MainLayout/>}>
+          <Route path="/" element={<HomePage />}/>
+            
+        </Route>
       </Routes>
     </>
   )
