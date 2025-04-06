59a6da5840f4ba533238b4faa3d2a582c7122232
Seigha Rufus <excellenceseigha@gmail.com>
2025-04-06 23:03:49 +0100

    add useMusicStore to codebase

--- frontend/src/Layout/components/LeftSideBar.tsx
+++ frontend/src/Layout/components/LeftSideBar.tsx
@@ -2,13 +2,21 @@ import PlayListSkeleton from '@/components/skeletons/PlayListSkeleton'
 import { buttonVariants } from '@/components/ui/button'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { cn } from '@/lib/utils'
+import { useMusicStore } from '@/stores/useMusicStore'
 import { SignedIn } from '@clerk/clerk-react'
 import { HomeIcon, Library, MessageCircle } from 'lucide-react'
-import React from 'react'
+import { useEffect } from 'react'
 import { Link } from 'react-router'
 
 const LeftSideBar = () => {
-  const isLoading = true;
+  const { isLoading, albums, fetchAlbums } = useMusicStore();
+
+  useEffect(() => {
+    fetchAlbums();
+  },[fetchAlbums]);
+
+  console.log({albums});
+
   return (
     <div className='h-full flex flex-col gap-2'>
       {/* navigation menu */}
