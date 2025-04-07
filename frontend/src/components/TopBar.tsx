4b11de928926654a90b50f0ffccf00f31fb17d04
Seigha Rufus <excellenceseigha@gmail.com>
2025-04-07 18:03:05 +0100

    complete TopBar component

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -1,24 +1,23 @@
-import { SignedIn, SignedOut, SignOutButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignOutButton, UserButton } from '@clerk/clerk-react'
 import { LayoutDashboardIcon } from 'lucide-react'
-import React from 'react'
 import { Link } from 'react-router'
 import SignInOAuthButton from './SignInOAuthButton'
 import { useAuthStore } from '@/stores/useAuthStore'
 
 const TopBar = () => {
-  const { isAdmin } = useAuthStore() // Replace with actual logic to determine if the user is an admin
-  console.log({isAdmin})
+  const { isAdmin } = useAuthStore()
   return (
-    <div className='flex justify-between items-center bg-zinc-900/75 p-4 sticky top-0 z-10 backdrop-blur-md'>
-      <div className='flex gap-2 items-center '>
+    <div className='flex justify-between items-center bg-zinc-900/75 p-4 sticky top-0 z-10 backdrop-blur-md rounded-md'>
+      <div className='flex gap-2 items-center'>
+        <img src="/spotify.png" alt="spotify logo" className='size-8'/>
         Rufify
       </div>
       <div className='flex items-center gap-4'>
         {isAdmin && (
-          <>
+          <div className='flex items-center justify-center bg-black p-1 px-2 rounded-xs hover:bg-black/30'>
             <LayoutDashboardIcon className='size-4 mr-2'/>
             <Link to={`/admin`}>Admin Dashboard</Link>
-          </>
+          </div>
         )}
 
         <SignedIn>
@@ -28,6 +27,8 @@ const TopBar = () => {
         <SignedOut>
           <SignInOAuthButton/>
         </SignedOut>
+
+        <UserButton/>
       </div>      
     </div>
   )
